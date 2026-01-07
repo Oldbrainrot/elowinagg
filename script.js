feb6acf83b79538a50c563a6ebb7c92e6d478cee
Elowinagg <sigma9skibidi@gmail.com>
2026-01-07 18:38:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const trigger = document.getElementById("trigger");
-
 trigger.addEventListener("click", () => {
-  window.open("popup.html", "_blank", "width=400,height=300");
+  sound.volume = 1;
+  sound.play();
+
+  const popupCount = 4;
+  for (let i = 0; i < popupCount; i++) {
+    window.open(
+      "popup.html",
+      "_blank",
+      `width=400,height=300,left=${50 + i*40},top=${50 + i*40}`
+    );
+  }
+
+  downloadFile("media/you.zip");
 });
